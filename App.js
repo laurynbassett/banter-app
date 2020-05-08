953b70065d3290650462226b8df45fb486a0c137
Isra Khan <israkhan2@gmail.com>
2020-05-08 13:42:14 -0500

    ignoring eslint errorC

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
 import { SplashScreen } from "expo";
-import * as Font from "expo-font";
+// import * as Font from "expo-font";
 // import { Ionicons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
