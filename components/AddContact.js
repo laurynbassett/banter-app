0cb6c1920adf57a4d8f7cce26f22c5e3745d2809
Lauryn Bassett <lauryn.bassett@gmail.com>
2020-05-20 07:16:03 -0500

    removed add contact name field

--- components/AddContact.js
+++ components/AddContact.js
@@ -1,93 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { connect } from "react-redux";
 
-import { addNewContact, fetchUser } from '../store';
+import { addNewContact, fetchUser } from "../store";
 
 const AddContact = props => {
-	const [ name, setName ] = useState('');
-	const [ email, setEmail ] = useState('');
+  const [ email, setEmail ] = useState("");
 
-	const fetchData = async () => {
-		await props.fetchUser;
-	};
+  const fetchData = async () => {
+    await props.fetchUser;
+  };
 
-	useEffect(() => {
-		fetchData();
-	});
+  useEffect(() => {
+    fetchData();
+  });
 
-	const handleAdd = () => {
-		props.addNewContact({ name, email }, props.navigation);
-		setName('');
-		setEmail('');
-	};
+  const handleAdd = () => {
+    props.addNewContact({ email }, props.navigation);
+    setEmail("");
+  };
 
-	return (
-		<View style={styles.container}>
-			<View style={styles.form}>
-				<View style={styles.input}>
-					<TextInput
-						placeholder='Name'
-						style={styles.text}
-						value={name}
-						onChangeText={value => setName(value)}
-					/>
-				</View>
-				<View style={styles.input}>
-					<TextInput
-						autoCapitalize='none'
-						placeholder='Email'
-						style={styles.text}
-						value={email}
-						onChangeText={value => setEmail(value)}
-					/>
-				</View>
-			</View>
-			<View style={styles.buttonTextWrapper}>
-				<Button title='Add Contact' style={styles.button} onPress={handleAdd} />
-				<View style={styles.buttonTextWrapper}>
-					<Text style={[ styles.text, { color: 'red' } ]}>{props.addContactError}</Text>
-				</View>
-			</View>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <View style={styles.input}>
+          <TextInput
+            autoCapitalize='none'
+            placeholder='Email'
+            style={styles.text}
+            value={email}
+            onChangeText={value => setEmail(value)}
+          />
+        </View>
+      </View>
+      <View style={styles.buttonTextWrapper}>
+        <Button title='Add Contact' style={styles.button} onPress={handleAdd} />
+        <View style={styles.buttonTextWrapper}>
+          <Text style={[ styles.text, { color: "red" } ]}>{props.addContactError}</Text>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 const mapState = state => ({
-	user: state.user,
-	addContactError: state.user.addContactError
+  user: state.user,
+  addContactError: state.user.addContactError
 });
 
 const mapDispatch = dispatch => ({
-	addNewContact: (contact, navigation) => dispatch(addNewContact(contact, navigation)),
-	fetchUser: () => dispatch(fetchUser())
+  addNewContact: (contact, navigation) => dispatch(addNewContact(contact, navigation)),
+  fetchUser: () => dispatch(fetchUser())
 });
 
 export default connect(mapState, mapDispatch)(AddContact);
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		padding: 15
-	},
-	form: {
-		justifyContent: 'center'
-	},
-	input: {
-		margin: 10,
-		padding: 10,
-		backgroundColor: '#fff',
-		borderRadius: 10
-	},
-	text: {
-		fontSize: 16,
-		height: 40
-	},
-	button: {
-		flex: 1
-	},
-	buttonTextWrapper: {
-		marginTop: 20,
-		alignItems: 'center'
-	}
+  container: {
+    flex: 1,
+    padding: 15
+  },
+  form: {
+    justifyContent: "center"
+  },
+  input: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: "#fff",
+    borderRadius: 10
+  },
+  text: {
+    fontSize: 16,
+    height: 40
+  },
+  button: {
+    flex: 1
+  },
+  buttonTextWrapper: {
+    marginTop: 20,
+    alignItems: "center"
+  }
 });
